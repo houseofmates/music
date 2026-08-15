984f7942eef10aa1cb65d3ef1f4288aa2479b091
pkm-sync <house@houseofmates.space>
2026-08-15 11:57:13 -0700

    autosync: 2026-08-15T11:57:13

--- android/app/src/main/java/com/house/music/MainActivity.java
+++ android/app/src/main/java/com/house/music/MainActivity.java
@@ -13,7 +13,6 @@ import android.provider.Settings;
 import android.util.Log;
 import android.view.View;
 import android.view.WindowManager;
-import android.view.WindowInsets;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
